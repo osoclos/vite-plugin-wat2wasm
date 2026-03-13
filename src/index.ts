754b1e4e8c560c825de5de75151a7c35ef0331cb
osoclos <osoclosus@gmail.com>
2026-03-13 21:58:05 +0800

    added better error handling for non-compilation errors

--- src/index.ts
+++ src/index.ts
@@ -135,7 +135,19 @@ const watCompilerPlugin = (options: Wat2WasmOptions = {}): Plugin => {
                 "" + "\n" +
                 "const init = async (imports = {}) => {" + "\n" +
                 "    return WebAssembly.instantiate(new Uint8Array(str.length).fill(0x00).map((_, i) => str.charCodeAt(i)).buffer, imports).then(({ instance: { exports } }) => exports).catch(({ message }) => {" + "\n" +
-                "        const [_, ptr] = message.match(/ @\\+(\\d+)\\s*$/);" + "\n" +
+                "        if (message === undefined || typeof message !== \"string\") {" + "\n" +
+                "            console.error(err[Symbol.toStringTag]() === \"WebAssembly.Exception\" ? \"Exception tag from WebAssembly file has been thrown while instantiating.\" : \"Unknown error while instantiating WebAssembly file.\");" + "\n" +
+                "            return;" + "\n" +
+                "        }" + "\n" +
+                "        " + "\n" +
+                "        const res = message.match(/ @\\+(\\d+)\\s*$/);" + "\n" +
+                "        " + "\n" +
+                "        if (res === null) {" + "\n" +
+                "            console.error(message);" + "\n" +
+                "            return;" + "\n" +
+                "        }" + "\n" +
+                "        " + "\n" +
+                "        const [_, ptr] = res;" + "\n" +
                 "        console.error(message + \": \" + [...new TextEncoder().encode(str.slice(+ptr - 40, +ptr + 40))].map((byte, i) => (i === 40 ? \"[\" : \"\") + byte.toString(16).padStart(2, \"0\") + (i === 40 ? \"]\" : \"\")).join(\" \"));" + "\n" +
                 "    });" + "\n" +
                 "}" + "\n" +
