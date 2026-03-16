82a2498dffd8177369051881b7686af71309edd0
osoclos <osoclosus@gmail.com>
2026-03-16 14:04:46 +0800

    added types from v1.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-export function add(a: number, b: number): number {
-    return a + b;
+import { Plugin, ResolvedConfig } from "vite";
+import initWabt from "wabt";
+
+const wabt = await initWabt();
+
+type WabtParserFunc = Awaited<ReturnType<typeof initWabt>>["parseWat"];
+
+/** See @see {@link https://github.com/AssemblyScript/wabt.js/blob/main/README.md|`wabt.WasmFeatures`} for more info. @see WasmParserOptions */
+type WasmParserOptions = Parameters<WabtParserFunc>[2];
+
+/** See @see {@link https://github.com/AssemblyScript/wabt.js/blob/main/README.md|`wabt.ToBinaryOptions`} for more info. @see WasmGeneratorOptions */
+type WasmGeneratorOptions = Parameters<ReturnType<WabtParserFunc>["toBinary"]>[0];
+
+/** The configuration settings for `vite-plugin-wat2wasm`. @see Wat2WasmOptions */
+interface Wat2WasmOptions {
+    /** Configures `.wasm` features you wish to enable for `vite-plugin-wat2wasm`. @default {} @see {@link WasmParserOptions|`WasmParserOptions`} */
+    parser?: WasmParserOptions;
+
+    /** Configures how `vite-plugin-wat2wasm` to generate `.wasm` files. @default {} @see {@link WasmGeneratorOptions|`WasmGeneratorOptions`} */
+    generator?: WasmGeneratorOptions;
 }
+
+const wat2WasmPlugin = (): Plugin => {
+    let config: ResolvedConfig;
+
+    return {
+        name: "wat2wasm",
+
+        transform(code: string, id: string) {
+            if (!id.endsWith(".wat")) return null;
+
+
+        },
+
+        configResolved: (resolvedConfig: ResolvedConfig) => void (config = resolvedConfig)
+    };
+};
+
+export default wat2WasmPlugin;
