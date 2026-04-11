c226153197a11b9696ef66b47a4958ef26292511
osoclos <osoclosus@gmail.com>
2026-04-12 00:17:41 +0800

    fixed spacing in types

--- src/types-next.d.ts
+++ src/types-next.d.ts
@@ -2,6 +2,6 @@ declare module "*.wat" {
     // @ts-ignore
     type ModuleObject = Record<string, any>;
 
-    export default function init<T extends ModuleObject = ModuleObject                             >(                                                       ): Promise<T>;
+    export default function init<T extends ModuleObject = ModuleObject                             >(                                                                  ): Promise<T>;
     export default function init<T extends ModuleObject = ModuleObject, I extends ModuleObject = {}>(imports: I, compileOptions?: WebAssembly.WebAssemblyCompileOptions): Promise<T>;
 }
