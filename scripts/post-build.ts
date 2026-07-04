0d80eb3dd379ff63fcf3afed1df167fabf496606
osoclos <osoclosus@gmail.com>
2026-07-04 16:38:21 +0800

    switched to xcopy for copying files

--- scripts/post-build.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-copyFile(path.join(__dirname, "../src/types.d.ts"), path.join(__dirname, "../dist/types.d.ts"));
-copyFile(path.join(__dirname, "../src/types-next.d.ts"), path.join(__dirname, "../dist/types-next.d.ts"));
-
-function copyFile(pathIn: string, pathOut: string) {
-    fs.writeFileSync(pathOut, fs.readFileSync(pathIn));
-}
